58a3faa750d1c2a0df212346439d359e4326582d
Salvo <pellegrino.salvatore02@gmail.com>
2023-03-07 12:07:45 +0100

    Add exercise _3

---
+++ _3/Main.java
@@ -0,0 +1,62 @@
+package exercise.javabasics._3;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    /**
+     * 1: print out your initials using System.out.print and then a char literal,
+     * i.e. 'a', 'b', 'c'. You will need multiple print statements
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Write your code here
+        System.out.print('S');
+        System.out.println('P');
+
+    }
+
+    /**
+     * 2: Print out your age as an int literal, i.e. 28, then print whether or not you've
+     * had lunch today as a boolean literal i.e. true, false, then print the price of
+     * your lunch as a double, i.e. 4.99
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        // Write your code here
+        System.out.println("My age is= ");
+        System.out.println(34);
+        System.out.println("Had I Lunch? ");
+        System.out.println(true);
+        System.out.println("Price of lunch= ");
+        System.out.println(3.54);
+
+    }
+
+    /**
+     * 3: Complete exercise 2, but store the values in a variable.
+     * And then print out the variable.
+     *
+     * i.e.
+     * char favouriteLetter = 'g';
+     * System.out.print("My favourite letter=");
+     * System.out.println(favouriteLetter)
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        // Write your code here
+        int myAge = 34;
+        boolean hadILunch = true;
+        double priceOfLunch = 3.54;
+
+        System.out.println("My age is= ");
+        System.out.println(myAge);
+        System.out.println("Had I Lunch? ");
+        System.out.println(hadILunch);
+        System.out.println("Price of lunch= ");
+        System.out.println(priceOfLunch);
+    }
+}
